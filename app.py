1b72b903bde91411cd551877e649c81f255e345e
Luciano Carbonera <luciano.c10@aluno.ifsc.edu.br>
2022-05-06 20:25:52 -0300

    update app

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.write("# Classificação de Iris")
 st.write("## Exemplo com comprimentos de pétala e sépala")
 
 st.sidebar.write("### Parâmetros")
-st.sidebar.slider("Comprimento de sépala", 4, 8, 5.8, 0.1)
-st.sidebar.slider("Comprimento de sépala", 0.9, 7, 3.8, 0.1)
+st.sidebar.slider("Comprimento de sépala", 4.0, 8.0, 5.8, 0.1)
+st.sidebar.slider("Comprimento de sépala", 0.9, 7.0, 3.8, 0.1)
