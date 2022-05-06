060c5aa1d48d22691a341a74d934ef1cae1bf6f3
Luciano Carbonera <luciano.c10@aluno.ifsc.edu.br>
2022-05-06 20:25:06 -0300

    app

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+%%writefile app.py
 import streamlit as st
 
 st.write("# Classificação de Iris")
 st.write("## Exemplo com comprimentos de pétala e sépala")
+
+st.sidebar.write("### Parâmetros")
+st.sidebar.slider("Comprimento de sépala", 4, 8, 5.8, 0.1)
+st.sidebar.slider("Comprimento de sépala", 0.9, 7, 3.8, 0.1)
